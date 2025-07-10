5bc099fb32486e11fe930672eb12605881a29f34
jothikumar-stack <sjothikumar54@gmail.com>
2025-07-10 09:34:22 +0530

    text changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React + Docker
+          Welcome to React + Docker world
         </a>
       </header>
     </div>
